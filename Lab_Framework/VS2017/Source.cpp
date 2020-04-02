a2d9405fd737264d51aae58804207c51b636fba5
George <gdmavroeidis@hotmail.com>
2020-04-02 15:51:23 -0400

    fixed light and textures WOW

---
+++ Lab_Framework/VS2017/Source.cpp
@@ -0,0 +1,24 @@
+/*
+#include <iostream>
+#include <list>
+
+#define GLEW_STATIC 1   // This allows linking with Static Library on Windows, without DLL
+#include <GL/glew.h>    // Include GLEW - OpenGL Extension Wrangler
+
+#include <GLFW/glfw3.h> // cross-platform interface for creating a graphical context,
+// initializing OpenGL and binding inputs
+
+#include <glm/glm.hpp>  // GLM is an optimized math library with syntax to similar to OpenGL Shading Language
+#include <glm/gtc/matrix_transform.hpp> // include this to create transformation matrices
+#include <glm/common.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "OBJloader.h"
+
+using namespace glm;
+using namespace std;
+
+int numOfVertices;
+float fov = 70.0f;
+*/
